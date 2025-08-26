d399f77a14d6be6ea50cfe9f86736e421882d7d4
Akimkerrache <khaknav@gmail.com>
2025-08-26 15:21:35 +0100

    vercel.json

--- app/[locale]/admin/web-pages/web-page-form.tsx
+++ app/[locale]/admin/web-pages/web-page-form.tsx
@@ -1,15 +1,11 @@
-'use client'
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useRouter } from 'next/navigation'
-import { useForm } from 'react-hook-form'
-
-import { z } from 'zod'
-
-import MdEditor from 'react-markdown-editor-lite'
-import ReactMarkdown from 'react-markdown'
-import 'react-markdown-editor-lite/lib/index.css'
-import { Button } from '@/components/ui/button'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,77 +13,80 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { useToast } from '@/hooks/use-toast'
-import { createWebPage, updateWebPage } from '@/lib/actions/web-page.actions'
-import { IWebPage } from '@/lib/db/models/web-page.model'
-import { WebPageInputSchema, WebPageUpdateSchema } from '@/lib/validator'
-import { Checkbox } from '@/components/ui/checkbox'
-import { toSlug } from '@/lib/utils'
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
+import { createWebPage, updateWebPage } from "@/lib/actions/web-page.actions";
+import { IWebPage } from "@/lib/db/models/web-page.model";
+import { WebPageInputSchema, WebPageUpdateSchema } from "@/lib/validator";
+import { Checkbox } from "@/components/ui/checkbox";
+import { toSlug } from "@/lib/utils";
 
 const webPageDefaultValues =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? {
-        title: 'Sample Page',
-        slug: 'sample-page',
-        content: 'Sample Content',
+        title: "Sample Page",
+        slug: "sample-page",
+        content: "Sample Content",
       }
     : {
-        title: '',
-        slug: '',
-        content: '',
-      }
+        title: "",
+        slug: "",
+        content: "",
+      };
 
 const WebPageForm = ({
   type,
   webPage,
   webPageId,
 }: {
-  type: 'Create' | 'Update'
-  webPage?: IWebPage
-  webPageId?: string
+  type: "Create" | "Update";
+  webPage?: IWebPage;
+  webPageId?: string;
 }) => {
-  const router = useRouter()
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof WebPageInputSchema>>({
     resolver:
-      type === 'Update'
+      type === "Update"
         ? zodResolver(WebPageUpdateSchema)
         : zodResolver(WebPageInputSchema),
     defaultValues:
-      webPage && type === 'Update' ? webPage : webPageDefaultValues,
-  })
+      webPage && type === "Update" ? webPage : webPageDefaultValues,
+  });
 
-  const { toast } = useToast()
+  const { toast } = useToast();
   async function onSubmit(values: z.infer<typeof WebPageInputSchema>) {
-    if (type === 'Create') {
-      const res = await createWebPage(values)
+    if (type === "Create") {
+      const res = await createWebPage(values);
       if (!res.success) {
         toast({
-          variant: 'destructive',
+          variant: "destructive",
           description: res.message,
-        })
+        });
       } else {
         toast({
           description: res.message,
-        })
-        router.push(`/admin/web-pages`)
+        });
+        router.push(`/admin/web-pages`);
       }
     }
-    if (type === 'Update') {
+    if (type === "Update") {
       if (!webPageId) {
-        router.push(`/admin/web-pages`)
-        return
+        router.push(`/admin/web-pages`);
+        return;
       }
-      const res = await updateWebPage({ ...values, _id: webPageId })
+      const res = await updateWebPage({ ...values, _id: webPageId });
       if (!res.success) {
         toast({
-          variant: 'destructive',
+          variant: "destructive",
           description: res.message,
-        })
+        });
       } else {
-        router.push(`/admin/web-pages`)
+        toast({
+          description: res.message,
+        });
+        router.push(`/admin/web-pages`);
       }
     }
   }
@@ -95,21 +94,20 @@ const WebPageForm = ({
   return (
     <Form {...form}>
       <form
-        method='post'
+        method="post"
         onSubmit={form.handleSubmit(onSubmit)}
-        className='space-y-8'
+        className="space-y-8"
       >
-        <div className='flex flex-col gap-5 md:flex-row'>
+        <div className="flex flex-col gap-5 md:flex-row">
           <FormField
             control={form.control}
-            name='title'
+            name="title"
             render={({ field }) => (
-              <FormItem className='w-full'>
-                <FormLabel>Title</FormLabel>
+              <FormItem className="w-full">
+                <FormLabel>Titre</FormLabel>
                 <FormControl>
-                  <Input placeholder='Enter title' {...field} />
+                  <Input placeholder="Entrez le titre" {...field} />
                 </FormControl>
-
                 <FormMessage />
               </FormItem>
             )}
@@ -117,52 +115,49 @@ const WebPageForm = ({
 
           <FormField
             control={form.control}
-            name='slug'
+            name="slug"
             render={({ field }) => (
-              <FormItem className='w-full'>
+              <FormItem className="w-full">
                 <FormLabel>Slug</FormLabel>
-
                 <FormControl>
-                  <div className='relative'>
+                  <div className="relative">
                     <Input
-                      placeholder='Enter slug'
-                      className='pl-8'
+                      placeholder="Entrez le slug"
+                      className="pl-8"
                       {...field}
                     />
-                    <button
-                      type='button'
+                    <Button
+                      type="button"
+                      variant="outline"
+                      size="sm"
+                      className="absolute right-2 top-2.5"
                       onClick={() => {
-                        form.setValue('slug', toSlug(form.getValues('title')))
+                        form.setValue("slug", toSlug(form.getValues("title")));
                       }}
-                      className='absolute right-2 top-2.5'
                     >
-                      Generate
-                    </button>
+                      Générer
+                    </Button>
                   </div>
                 </FormControl>
-
                 <FormMessage />
               </FormItem>
             )}
           />
         </div>
-        <div className='flex flex-col gap-5 md:flex-row'>
+        <div className="flex flex-col gap-5 md:flex-row">
           <FormField
             control={form.control}
-            name='content'
+            name="content"
             render={({ field }) => (
-              <FormItem className='w-full'>
-                <FormLabel>Content</FormLabel>
+              <FormItem className="w-full">
+                <FormLabel>Contenu</FormLabel>
                 <FormControl>
-                  <MdEditor
-                    // value={markdown}
-                    {...field}
-                    style={{ height: '500px' }}
-                    renderHTML={(text) => <ReactMarkdown>{text}</ReactMarkdown>}
-                    onChange={({ text }) => form.setValue('content', text)}
+                  <MDEditor
+                    value={field.value}
+                    onChange={(value) => form.setValue("content", value || "")}
+                    height={500}
+                    preview="edit"
                   />
-
-                  {/* <Textarea placeholder='Enter content' {...field} /> */}
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -172,33 +167,35 @@ const WebPageForm = ({
         <div>
           <FormField
             control={form.control}
-            name='isPublished'
+            name="isPublished"
             render={({ field }) => (
-              <FormItem className='space-x-2 items-center'>
+              <FormItem className="space-x-2 items-center">
                 <FormControl>
                   <Checkbox
                     checked={field.value}
                     onCheckedChange={field.onChange}
                   />
                 </FormControl>
-                <FormLabel>Is Published?</FormLabel>
+                <FormLabel>Publier la page ?</FormLabel>
               </FormItem>
             )}
           />
         </div>
         <div>
           <Button
-            type='submit'
-            size='lg'
+            type="submit"
+            size="lg"
             disabled={form.formState.isSubmitting}
-            className='button col-span-2 w-full'
+            className="button col-span-2 w-full"
           >
-            {form.formState.isSubmitting ? 'Submitting...' : `${type} Page `}
+            {form.formState.isSubmitting
+              ? "Enregistrement..."
+              : `${type === "Create" ? "Créer" : "Mettre à jour"} Page`}
           </Button>
         </div>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default WebPageForm
+export default WebPageForm;
