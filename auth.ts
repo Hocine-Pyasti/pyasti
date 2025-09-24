875584f33c8ac597cf78b1f4118cc7d0e5e2cd44
Akimkerrache <khaknav@gmail.com>
2025-09-24 19:45:55 +0100

    auth

--- auth.ts
+++ auth.ts
@@ -19,6 +19,7 @@ declare module "next-auth" {
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
   pages: {
     signIn: "/sign-in",
     newUser: "/sign-up",
