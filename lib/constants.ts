9117eba7c15462cc8e93f6852acdc028af5134be
Akimkerrache <khaknav@gmail.com>
2025-09-24 23:43:15 +0100

    admin email

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 export const SENDER_NAME = process.env.SENDER_NAME || "PYASTI support";
 export const SENDER_EMAIL = process.env.SENDER_EMAIL || "pyasti.dz@gmail.com";
-export const ADMIN_EMAIL =
-  process.env.ADMIN_EMAIL || "admin.pyasti.dz@gmail.com";
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "pyasti.adz@gmail.com";
 
 export const USER_ROLES = ["Admin", "Seller", "User"];
 export const COLORS = ["Gold", "Green", "Red", "Blue"];
